028553855c3f74087b90416b3d91bc9b5b732ecd
j1nxie <rylieeeeexd@gmail.com>
2023-03-09 02:43:25 +0700

    feat(main.rs): implement chapter 01 exercise

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
+use crate::tuple::Tuple;
+
 mod tuple;
 
+#[derive(Debug)]
+struct Projectile {
+    position: Tuple,
+    velocity: Tuple,
+}
+
+impl Projectile {
+    fn tick(&mut self, environment: &Environment) {
+        *self = Self {
+            position: self.position + self.velocity,
+            velocity: self.velocity + environment.gravity + environment.wind,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Environment {
+    gravity: Tuple,
+    wind: Tuple,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut projectile = Projectile {
+        position: Tuple::point(0.0, 1.0, 0.0),
+        velocity: Tuple::vector(1.0, 1.0, 0.0).normalize(),
+    };
+
+    let environment = Environment {
+        gravity: Tuple::vector(0.0, -0.1, 0.0),
+        wind: Tuple::vector(-0.01, 0.0, 0.0),
+    };
+
+    while projectile.position.y > 0.0 {
+        println!("current y position: {:.}", projectile.position.y);
+        projectile.tick(&environment);
+    }
 }
