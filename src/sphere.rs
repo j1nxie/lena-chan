b652db7ac541236c24d2efc933163e66521af898
j1nxie <rylieeeeexd@gmail.com>
2023-04-29 20:01:49 +0700

    test: tests for intersection

--- src/sphere.rs
+++ src/sphere.rs
@@ -12,10 +12,54 @@ impl Sphere {
 
 #[cfg(test)]
 mod tests {
+    use crate::tuple::Tuple;
+
     use super::*;
 
     #[test]
     fn test_create_sphere() {
         let sphere = Sphere::new();
+    }
+
+    #[test]
+    fn test_intersect() {
+        let r = Ray::new(Tuple::point(0.0, 1.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0], 5.0);
+        assert_eq!(xs[1], 5.0);
+    }
+
+    #[test]
+    fn test_intersect_miss() {
+        let r = Ray::new(Tuple::point(0.0, 2.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 0);
+    }
+
+    #[test]
+    fn test_ray_inside_sphere() {
+        let r = Ray::new(Tuple::point(0.0, 0.0, 0.0), Tuple::vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0], -1.0);
+        assert_eq!(xs[1], 1.0);
+    }
+
+    #[test]
+    fn test_ray_behind_sphere() {
+        let r = Ray::new(Tuple::point(0.0, 0.0, 5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0], -6.0);
+        assert_eq!(xs[1], -4.0);
     }
 }
