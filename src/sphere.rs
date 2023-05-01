e6ed1f1e438bb6689755d02e5d1835b8e61ed0e6
j1nxie <rylieeeeexd@gmail.com>
2023-05-02 02:50:50 +0700

    feat: implement Intersection struct

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,5 +1,10 @@
+use std::f64::EPSILON;
+
+use float_eq::float_eq;
+
 use crate::{ray::Ray, tuple::Tuple};
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Sphere;
 
 impl Sphere {
@@ -7,7 +12,7 @@ impl Sphere {
         Sphere
     }
 
-    pub fn intersect(&self, ray: Ray) -> Vec<f64> {
+    pub fn intersect(&self, ray: Ray) -> Vec<Intersection> {
         let sphere_to_ray = ray.origin - Tuple::point(0.0, 0.0, 0.0);
         let a = ray.direction * ray.direction;
         let b = 2.0 * (ray.direction * sphere_to_ray);
@@ -17,16 +22,51 @@ impl Sphere {
         if discriminant < 0.0 {
             vec![]
         } else {
-            let t1 = (-b - f64::sqrt(discriminant)) / (2.0 * a);
-            let t2 = (-b + f64::sqrt(discriminant)) / (2.0 * a);
+            let t1 = Intersection::new(
+                (-b - f64::sqrt(discriminant)) / (2.0 * a),
+                Object::Sphere(*self),
+            );
+            let t2 = Intersection::new(
+                (-b + f64::sqrt(discriminant)) / (2.0 * a),
+                Object::Sphere(*self),
+            );
 
             vec![t1, t2]
         }
     }
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Object {
+    Sphere(Sphere),
+}
+
+#[derive(Clone, Debug)]
+pub struct Intersection {
+    pub value: f64,
+    pub object: Object,
+}
+
+impl PartialEq for Intersection {
+    fn eq(&self, other: &Self) -> bool {
+        float_eq!(self.value, other.value, abs <= EPSILON) && self.object == other.object
+    }
+}
+
+impl Eq for Intersection {}
+
+impl Intersection {
+    pub fn new(value: f64, object: Object) -> Self {
+        Self { value, object }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::f64::EPSILON;
+
+    use float_eq::assert_float_eq;
+
     use super::*;
 
     #[test]
@@ -41,8 +81,8 @@ mod tests {
         let xs = s.intersect(r);
 
         assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], 5.0);
-        assert_eq!(xs[1], 5.0);
+        assert_eq!(xs[0].value, 5.0);
+        assert_eq!(xs[1].value, 5.0);
     }
 
     #[test]
@@ -61,8 +101,8 @@ mod tests {
         let xs = s.intersect(r);
 
         assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], -1.0);
-        assert_eq!(xs[1], 1.0);
+        assert_eq!(xs[0].value, -1.0);
+        assert_eq!(xs[1].value, 1.0);
     }
 
     #[test]
@@ -72,7 +112,16 @@ mod tests {
         let xs = s.intersect(r);
 
         assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], -6.0);
-        assert_eq!(xs[1], -4.0);
+        assert_eq!(xs[0].value, -6.0);
+        assert_eq!(xs[1].value, -4.0);
+    }
+
+    #[test]
+    fn test_intersection_object() {
+        let s = Sphere::new();
+        let i = Intersection::new(3.5, Object::Sphere(s));
+
+        assert_float_eq!(i.value, 3.5, abs <= EPSILON);
+        assert_eq!(i.object, Object::Sphere(s));
     }
 }
